70517484754e14f8a0810c22ba5e293742924a4a
Zoebusigny <zoe.busigny@studentfr.ch>
2024-11-12 16:44:06 +0100

    creation de la base du scrolltrigger horizontal

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-import gsap from "gsap";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const contents = gsap.utils.toArray("#horizontal .section");
+
+gsap.to(contents, {
+  xPercent: -100 * (contents.length - 1), //deplacement de l'élément
+  scrollTrigger: {
+    trigger: "#horizontal",
+    pin: true, //épingle l'élément a sa position actuelle
+    scrub: 1, //la valeur définir un delais à l'animation
+  },
+});
+
+//Cette animation a été crée afin de presenté des personnage a l'horizontal pour
