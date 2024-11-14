e12d065c8c404c9e45b44c3f120cbb232f77b7f1
Zoebusigny <zoe.busigny@studentfr.ch>
2024-11-14 21:37:09 +0100

    recommencer le css et le javascript depuis le debutt

--- src/js/main.js
+++ src/js/main.js
@@ -19,153 +19,165 @@ gsap.to(contents, {
 
 //family-paint
 
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".family-paint-bulle-1",
-      start: " bottom bottom ",
-      //   end: "top center",
-      scrub: 0.5,
-    },
-  })
-  .to(".family-paint-bulle-1", {
-    opacity: 1,
-    x: "15%",
-  })
-  .to(
-    ".family-paint-bulle-1",
-    {
-      opacity: 0,
-    },
-    "+=0.5"
-  );
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".family-paint-bulle-1",
+//       start: " bottom bottom ",
+//       //   end: "top center",
+//       scrub: 0.5,
+//     },
+//   })
+//   .to(".family-paint-bulle-1", {
+//     opacity: 1,
+//     x: "15%",
+//   })
+//   .to(
+//     ".family-paint-bulle-1",
+//     {
+//       opacity: 0,
+//     },
+//     "+=0.5"
+//   );
 
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".family-paint-bulle-2",
-      start: " bottom bottom ",
-      //   end: "top center",
-      scrub: 0.5,
-    },
-  })
-  .to(".family-paint-bulle-2", {
-    opacity: 1,
-    x: "-15%",
-  })
-  .to(
-    ".family-paint-bulle-2",
-    {
-      opacity: 0,
-    },
-    "+=0.5"
-  );
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".family-paint-bulle-2",
+//       start: " bottom bottom ",
+//       //   end: "top center",
+//       scrub: 0.5,
+//     },
+//   })
+//   .to(".family-paint-bulle-2", {
+//     opacity: 1,
+//     x: "-15%",
+//   })
+//   .to(
+//     ".family-paint-bulle-2",
+//     {
+//       opacity: 0,
+//     },
+//     "+=0.5"
+//   );
 
-//bed-room
+// //bed-room
 
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".bed-room-bulle-1",
-      start: " bottom bottom ",
-      //   end: "top center",
-      scrub: 0.5,
-    },
-  })
-  .to(".bed-room-bulle-1", {
-    opacity: 1,
-  })
-  .to(
-    ".bed-room-bulle-1",
-    {
-      opacity: 0,
-    },
-    "+=0.5"
-  );
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".bed-room-bulle-1",
+//       start: " bottom bottom ",
+//       //   end: "top center",
+//       scrub: 0.5,
+//     },
+//   })
+//   .to(".bed-room-bulle-1", {
+//     opacity: 1,
+//   })
+//   .to(
+//     ".bed-room-bulle-1",
+//     {
+//       opacity: 0,
+//     },
+//     "+=0.5"
+//   );
 
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".bed-room-bulle-2",
-      start: " center center ",
-      //   end: "top center",
-      scrub: 0.5,
-    },
-  })
-  .to(".bed-room-bulle-2", {
-    opacity: 1,
-  })
-  .to(
-    ".bed-room-bulle-2",
-    {
-      opacity: 1,
-    },
-    "+=0.5"
-  );
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".bed-room-bulle-2",
+//       start: " center center ",
+//       //   end: "top center",
+//       scrub: 0.5,
+//     },
+//   })
+//   .to(".bed-room-bulle-2", {
+//     opacity: 1,
+//   })
+//   .to(
+//     ".bed-room-bulle-2",
+//     {
+//       opacity: 0,
+//     },
+//     "+=0.5"
+//   );
 
-// //escape
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".escape-bulle-1",
-      start: " bottom bottom ",
-      //   end: "top center",
-      scrub: 0.5,
-    },
-  })
-  .to(".escape-bulle-1", {
-    opacity: 1,
-    x: "15%",
-  })
-  .to(
-    ".escape-bulle-1",
-    {
-      opacity: 01,
-    },
-    "+=0.5"
-  );
+// // //escape
 
-gsap.to(".escape-bulle-2", {
-  opacity: 1,
-  x: "-15%",
-  scrollTrigger: {
-    trigger: ".escape-bulle-2",
-    start: " bottom center",
-    end: "bottom bottom ",
-    scrub: 0.5,
-    markers: true,
-  },
-});
-// //alone bulle
-gsap.to(".alone-bulle-1", {
-  opacity: 1,
-  scrollTrigger: {
-    trigger: ".alone-bulle-1",
-    start: " bottom bottom ",
-    end: "center center",
-    scrub: 0.5,
-  },
-});
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".escape-bulle-1",
+//       start: " bottom bottom ",
+//       //   end: "top center",
+//       scrub: 0.5,
+//     },
+//   })
+//   .to(".escape-bulle-1", {
+//     opacity: 1,
+//     x: "15%",
+//   })
+//   .to(
+//     ".escape-bulle-1",
+//     {
+//       opacity: 0,
+//     },
+//     "+=0.5"
+//   );
 
-// //maxine
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".escape-bulle-2",
+//       start: " bottom bottom ",
+//       //   end: "top center",
+//       scrub: 0.5,
+//     },
+//   })
+//   .to(".escape-bulle-2", {
+//     opacity: 1,
+//     x: "-15%",
+//   })
+//   .to(
+//     ".escape-bulle-2",
+//     {
+//       opacity: 0,
+//     },
+//     "+=0.5"
+//   );
 
-gsap.to(".maxine-bulle-1", {
-  opacity: 1,
-  x: "20%",
-  scrollTrigger: {
-    trigger: ".maxine-bulle-1",
-    start: " bottom bottom ",
-    end: "top top",
-    scrub: 0.5,
-  },
-});
+// // //alone bulle
 
-gsap.to(".maxine-bulle-2", {
-  opacity: 1,
-  scrollTrigger: {
-    trigger: ".maxine-bulle-2",
-    start: " bottom bottom ",
-    end: "center center",
-    scrub: 0.5,
-  },
-});
+// gsap.to(".alone-bulle-1", {
+//   opacity: 1,
+//   scrollTrigger: {
+//     trigger: ".alone-bulle-1",
+//     start: " bottom bottom ",
+//     end: "center center",
+//     scrub: 0.5,
+//   },
+// });
+
+// // //maxine
+
+// gsap.to(".maxine-bulle-1", {
+//   opacity: 1,
+//   x: "20%",
+//   scrollTrigger: {
+//     trigger: ".maxine-bulle-1",
+//     start: " bottom bottom ",
+//     end: "top top",
+//     scrub: 0.5,
+//   },
+// });
+
+// gsap.to(".maxine-bulle-2", {
+//   opacity: 1,
+//   scrollTrigger: {
+//     trigger: ".maxine-bulle-2",
+//     start: " bottom bottom ",
+//     end: "center center",
+//     scrub: 0.5,
+//   },
+// });
